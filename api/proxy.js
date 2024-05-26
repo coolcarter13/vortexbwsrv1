80a3edb2e29dfa17b29a6e33c2be6d9c75d284c8
coolcarter13 <161138711+coolcarter13@users.noreply.github.com>
2024-05-25 20:01:34 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://scpwolf.booeducation.com";//your website url
+  let target = "https://smurfcat.isreally.pro/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
